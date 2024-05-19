ecf67f705536158a60ee7c3d2cd13bd2f5b388ab
EC2 Default User <ec2-user@ip-172-31-23-125.ap-northeast-1.compute.internal>
2024-05-19 07:58:21 +0000

    フラッシュメッセージの

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,35 +1,43 @@
 class BooksController < ApplicationController
-  
+
   def index
-    @books = Book.all  
+    @books = Book.all
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if  
+      @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to show_book_path(@book)
+    else
+      flash.now[:alert] = "投稿に失敗しました。" 
+      render :index
+    end
   end
 
   def show
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
     def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    flash[:notice] = "Book was successfully updated."
+    redirect_to show_book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books' 
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
   end
-  
+
   private
 
   def book_params
