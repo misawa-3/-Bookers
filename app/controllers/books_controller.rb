1e9d9fd542af8657e356e089d4076e8a47cf395d
EC2 Default User <ec2-user@ip-172-31-23-125.ap-northeast-1.compute.internal>
2024-05-19 09:07:42 +0000

    [Finish]RSpecテスト実施

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,13 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     if
       @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to show_book_path(@book)
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render :index
@@ -26,10 +26,14 @@ class BooksController < ApplicationController
   end
 
     def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to show_book_path(book.id)
+    @book = Book.find(params[:id])
+    if
+     @book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+  root 'homes#top'
+  
   get 'top' => 'homes#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'show_book'
+  get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
